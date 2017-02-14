db2c0958c8839362eadb5d81cb578ac157dd13e0
mounir <m.bouyamine17@gmail.com>
2017-02-14 10:42:52 +0100

    no message

--- src/algorithme/Algorithme.java
+++ src/algorithme/Algorithme.java
@@ -1,6 +1,35 @@
-package algorithme;
+package allocation;
 
 
-public interface Algorithme {
+import java.util.List;
 
+
+
+public interface algorithme {
+
+	public ur allouerUR(ur u);
+	
+	/*
+	 * Initialiser la liste des utilisateurs
+	 * */
+	
+	public void init(List<utilisateur> users);
+	
+	/*
+	 * Get & Set utilisateurs
+	 * */
+	
+	public List<utilisateur> getUtilisateurs();
+	
+	public void setUsers(List<utilisateur> users);
+	
+	public int getMknmoyenneProche();
+
+	public int getMknmoyenneLoin();
+
+
+	/**
+	 * Get nom d'algorithme
+	 */
+	public String getName();
 }
