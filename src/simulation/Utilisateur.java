5517ff1f6e4ca22f868fe43a9defcbd38620df08
mounir <m.bouyamine17@gmail.com>
2017-02-14 11:54:15 +0100

    no message

--- src/simulation/Utilisateur.java
+++ src/simulation/Utilisateur.java
@@ -1,12 +1,63 @@
-package simulation;
+package allocation;
 
 
+
+
+
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Utilisateur {
-	private LinkedList<Paquet> fifoEnvoi = new LinkedList<Paquet>();
-	private int distance; // [0..10]
-	
-	
+	// Parameters
+		private int id;
+		private List<Paquet> paquetAenvoyer;
+		private Deque<Paquet> fifo;
+		private PointAcces pA;
+		private int debitCourrant;
+		private int moyenProche;
+		
+
+		/**
+		 * Constructeur
+		 * 
+		 */
+		public Utilisateur(int id, PointAcces PointAcces, int mp) {
+
+			this.id = id;
+			this.pA = PointAcces;
+			this.moyenProche = mp;
+			this.paquetAenvoyer = new ArrayList<Paquet>();
+			fifo = new LinkedList<Paquet>();
+						
+		}
+
+		public void creerPaquet() {
+			
+			Paquet p = new Paquet(this, pA.getTemps());
+			fifo.add(p);
+				
+	}
+
+
+		public int getDebitCourrant() {
+			return debitCourrant;
+		}
+
+
+		public void setDebitCourrant(int debitCourrant) {
+			this.debitCourrant = debitCourrant;
+		}
+
+
+		public int getId() {
+			return id;
+		}
+
+
+		public List<Paquet> getPaquetAenvoyer() {
+			return paquetAenvoyer;
+		}
+
 }
- 
